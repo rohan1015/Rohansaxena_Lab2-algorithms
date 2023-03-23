7b7b3c041f3ea2f6d24567b467dbce11161f262c
rohan1015 <53273630+rohan1015@users.noreply.github.com>
2023-03-23 16:38:48 +0530

    Create PayMoneyService.java

---
+++ PayMoneyService.java
@@ -0,0 +1,18 @@
+package services;
+
+public class PayMoneyService {
+	public void inspect(int[] transactionArr, int targetValue) {
+		int no = transactionArr.length;
+		int sum = 0;
+		int i = 0;
+		while (sum <= targetValue && i < no) {
+			sum = sum + transactionArr[i];
+			i++;
+		}
+		if (sum >= targetValue) {
+			System.out.println("Target achieved after " + (i) + " transaction");
+		} else {
+			System.out.println("Given target is not achieved ");
+		}
+	}
+}
